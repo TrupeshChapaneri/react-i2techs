8fdb8fae49d49ae44ce121541e5ed6f3afc858d3
Trupesh Chapaneri <trupesh789@gmail.com>
2022-04-14 13:18:15 +0530

    Add: Comment Fun Added

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -4,9 +4,9 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-import { Collapse } from "@mui/material";
+import { Button, Collapse, FormGroup } from "@mui/material";
 import { callApi } from "../apiHelpers/ApiHelper";
-import { getComments } from "../apiHelpers/Url";
+import { getComments, addComment } from "../apiHelpers/Url";
 import { List, TextField } from "@material-ui/core";
 import { GlobalContext } from "../context/GlobalContext";
 import { CommentList } from "./CommentList";
@@ -20,6 +20,7 @@ function PostCard(props) {
   return (
     <Grid item xs={12} sm={12} md={12}>
       <Card
+        variant="outlined"
         sx={{
           height: "100%",
           display: "flex",
@@ -56,16 +57,42 @@ function PostCard(props) {
               <CommentList {...{ name, email, body }} />
             ))}
           </List>
-          <TextField
-            rows={2}
-            variant="filled"
-            multiline
-            value={commentText}
-            onChange={(e) => setCommentText(e.target.value)}
-            className="comment-input"
-            label="Enter Comment"
-            placeholder="Enter Comment"
-          />
+          <FormGroup className="comment-input">
+            <TextField
+              rows={2}
+              variant="filled"
+              multiline
+              value={commentText}
+              onChange={(e) => setCommentText(e.target.value)}
+              label="Enter Comment"
+              placeholder="Enter Comment"
+            />
+            <Button
+              disabled={commentText.length === 0}
+              variant="contained"
+              onClick={() => {
+                // here Using Hard Coded email nad name Becuse I did't get it API doc.
+                const data = {
+                  body: commentText,
+                  email: "hello@gmai.com",
+                  name: "Test User",
+                };
+                callApi({
+                  url: addComment(user_id),
+                  postData: data,
+                  onSuccess: (payload) => {
+                    setGlobalState((prv) => ({
+                      ...prv,
+                      postComment: [payload, ...prv.postComment],
+                    }));
+                    setCommentText("");
+                  },
+                });
+              }}
+            >
+              Add Comment
+            </Button>
+          </FormGroup>
         </Collapse>
       </Card>
     </Grid>
